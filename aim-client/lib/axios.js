9c5ee31bfad8dfcc5d93d2f1c35c5fa82d82dcaa
unknown <riiachoii@gmail.com>
2026-07-20 20:39:31 +0900

    feat: hero section layout added

--- aim-client/lib/axios.js
+++ aim-client/lib/axios.js
@@ -2,6 +2,7 @@
 
 import axios from "axios";
 import { getToken, removeToken } from "./token";
+import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
