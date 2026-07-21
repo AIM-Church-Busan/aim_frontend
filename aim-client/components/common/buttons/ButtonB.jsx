e7849e74e1db816f30d51c3a35bcb94de1da1d87
unknown <riiachoii@gmail.com>
2026-07-21 23:55:47 +0900

    fix(nav): navigation design changed

--- aim-client/components/common/buttons/ButtonB.jsx
+++ aim-client/components/common/buttons/ButtonB.jsx
@@ -10,7 +10,7 @@ export default function WatchDemoButton({ href, children = 'Watch Demo', classNa
     const content = (
         <>
             <span className="absolute inset-y-0 left-0 w-full origin-left scale-x-0 bg-tertiary transition-transform duration-300 ease-linear group-hover:scale-x-100" />
-            <span className="relative z-10 flex items-center gap-5">{children}</span>
+            <span className="relative z-10 flex items-center gap-5 font-anonymous font-semibold text-white">{children}</span>
         </>
     );
 
